7471196861abe993d0f046d37b57e6b9634725c5
Rodolfo Campos <camposer@gmail.com>
2015-03-05 16:41:56 +0100

    Solución del ejercicio1

---
+++ ejercicio1/ejercicio1/src/main/java/com/organizacion/ejercicio1/gui/CalculadoraGuiImpl.java
@@ -0,0 +1,46 @@
+package com.organizacion.ejercicio1.gui;
+
+import java.util.Scanner;
+
+import com.organizacion.ejercicio1.util.Calculadora;
+
+public class CalculadoraGuiImpl implements CalculadoraGui {
+	private Scanner scanner = new Scanner(System.in);
+	private Calculadora calc; // Inyectado por Spring!!
+	
+	public void setCalculadora(Calculadora calc) {
+		this.calc = calc;
+	}
+
+	public void iniciar() {
+		System.out.println("1. Sumar");
+		System.out.println("2. Restar");
+		System.out.println("3. Multiplicar");
+		System.out.println("4. Dividir");
+		System.out.print("? ");
+		
+		String operacion = scanner.nextLine();
+		
+		System.out.print("a? ");
+		float a = Float.parseFloat(scanner.nextLine());
+		System.out.print("b? ");
+		float  b = Float.parseFloat(scanner.nextLine());
+		
+		if (operacion.equals("1"))
+			System.out.println(a + " + " + b + " = " + calc.sumar(a, b));
+		else if (operacion.equals("2"))
+			System.out.println(a + " + " + b + " = " + calc.restar(a, b));
+		else if (operacion.equals("3"))
+			System.out.println(a + " * " + b + " = " + calc.multiplicar(a, b));
+		else
+			System.out.println(a + " * " + b + " = " + calc.dividir(a, b));
+	}
+}
+
+
+
+
+
+
+
+
